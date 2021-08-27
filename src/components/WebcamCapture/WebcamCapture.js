a7d8176e62a3364390fb6c7ad15ae00d2e58415c
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-27 19:44:07 +0800

    Added image capture functionality

--- src/components/WebcamCapture/WebcamCapture.js
+++ src/components/WebcamCapture/WebcamCapture.js
@@ -1,9 +1,17 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import "./WebcamCapture.css";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
 
 function WebcamCapture() {
   const webcamRef = useRef();
+  const [image, setImage] = useState(null);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+    console.log(imageSrc);
+  }, [webcamRef]);
 
   const videoConstraints = {
     width: 250,
@@ -21,6 +29,14 @@ function WebcamCapture() {
         width={videoConstraints.width}
         videoConstraints={videoConstraints}
       />
+
+      <RadioButtonUncheckedIcon
+        className="webcamCapture__button"
+        onClick={capture}
+        fontSize="large"
+      />
+
+      <img src={image} alt="ss" />
     </div>
   );
 }
