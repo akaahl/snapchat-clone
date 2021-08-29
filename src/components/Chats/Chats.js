e58a3e8c94afb530548f792bbb47ecbce88cde57
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-29 17:46:42 +0800

    Styled the Chats component, and extracted posts from db

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -1,10 +1,56 @@
-import React from "react";
+import { Avatar } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import "./Chats.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import { db } from "../../firebase/firebase";
 
 function Chats() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
   return (
-    <div>
-      <h2>Chats</h2>
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+
+        <div className="chats__search">
+          <SearchIcon />
+          <input type="text" placeholder="Friends" />
+        </div>
+
+        <ChatBubbleIcon className="chats__chatIcon" />
+      </div>
+
+      <div className="chats__posts">
+        {posts.map(
+          ({
+            id,
+            data: { profilePic, username, timestamp, imageUrl, read },
+          }) => (
+            <Chat
+              id={id}
+              key={id}
+              username={username}
+              imageUrl={imageUrl}
+              timestamp={timestamp}
+              read={read}
+              profilePic={profilePic}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 }
